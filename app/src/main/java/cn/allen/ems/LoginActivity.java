c292f47f8a9194a97e0a7f7372d6d6f93e1d00ba
allen <maojing644963131@126.com>
2020-10-15 08:55:48 +0800

    报错

--- app/src/main/java/cn/allen/ems/LoginActivity.java
+++ app/src/main/java/cn/allen/ems/LoginActivity.java
@@ -89,7 +89,8 @@ public class LoginActivity extends AllenIMBaseActivity {
         loginPhone.setText(shared.getString(Constants.User_Phone,""));
         loginPsw.setText(shared.getString(Constants.User_PassWord,""));
 //        loginPsw.setText("qwe!123");
-        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.CAMERA) !=PERMISSION_GRANTED||ActivityCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) !=PERMISSION_GRANTED||
+        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.CAMERA) !=PERMISSION_GRANTED||
+                ActivityCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) !=PERMISSION_GRANTED||
                 ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_WIFI_STATE) !=PERMISSION_GRANTED||
                 ActivityCompat.checkSelfPermission(this,Manifest.permission.READ_PHONE_STATE) !=PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE,
